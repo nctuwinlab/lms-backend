54739f2bcd3b4f8742411e561de8f091163bf84f
yubin551 <marks1994abc@gmail.com>
2017-07-31 00:46:52 +0800

    Create home model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Page, Article
+
 
 # Register your models here.
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'content']
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'content']
--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.db import models
+from member.models import Profile
+
 
 # Create your models here.
+class Page(models.Model):
+    subject = models.CharField(max_length=50, blank=False, default='標題')
+    author = models.ForeignKey(Profile)
+    datetime = models.DateTimeField(blank=False)
+    content = models.TextField()
+
+
+class Article(models.Model):
+    type = models.CharField(max_length=2, blank=False, default='一般')
+    subject = models.CharField(max_length=50)
+    author = models.ForeignKey(Profile)
+    datetime = models.DateTimeField(blank=False)
+    content = models.TextField()
