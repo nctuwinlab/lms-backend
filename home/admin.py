8ec78347f2b40c125906f6880a82eb8f36ec512c
aweimeow <aweimeow.tw@gmail.com>
2017-08-17 21:48:45 +0800

    Update Field Name from `subject` to `title`

--- home/admin.py
+++ home/admin.py
@@ -5,9 +5,9 @@ from .models import Page, Article
 # Register your models here.
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    list_display = ['subject', 'content']
+    list_display = ['title', 'content']
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['subject', 'content']
+    list_display = ['title', 'content']
